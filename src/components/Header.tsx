92e341a6a08fa660bd9d6c440ce74293a905479c
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-06-14 01:53:48 -0400

    Implement headers redirect to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { FC } from "react";
 import logo from "../assets/hacker-news.png";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full shadow-title py-[44px] bg-title">
       <div className="w-full container px-[150px] mx-auto">
-        <img src={logo} className="w-[208px] y-[28px]" />
+        <img
+          src={logo}
+          className="w-[208px] y-[28px]"
+          onClick={() => navigate("/")}
+        />
       </div>
     </div>
   );
