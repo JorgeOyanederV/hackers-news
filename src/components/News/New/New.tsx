cd96b5fb25c442551b7f302d7b270dc6f546d74d
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-06-14 02:49:04 -0400

    Hover and Cursor on New

--- src/components/News/New/New.tsx
+++ src/components/News/New/New.tsx
@@ -12,8 +12,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 export const New: FC = ({ _new }) => {
   const navigate = useNavigate();
-  const { author, story_title, created_at, objectID, isFaves } =
-    _new;
+  const { author, story_title, created_at, objectID, isFaves } = _new;
   const { loadingButtons } = useSelector((state: RootStore) => state.news);
   const [isFave, setIsFave] = useState(isFaves);
   const dispatch = useDispatch();
@@ -38,7 +37,7 @@ export const New: FC = ({ _new }) => {
     navigate(`/news/${_new.objectID}`, { state: { new: _new } });
   };
   return (
-    <div className="w-full md:h-[90px] pl-[26px] flex border border-[#979797] rounded-[6px] ">
+    <div className="w-full md:h-[90px] pl-[26px] flex border border-[#979797] rounded-[6px] hover:opacity-50 cursor-pointer">
       {/* When click on this redirect to the main new post */}
       <div
         className="flex-1 flex flex-col justify-center py-2"
